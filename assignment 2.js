ec2c86bd2e6eac3bee7b544120f426defa335d06
michellin0305 <mickrixtian@gmail.com>
2019-01-23 15:16:57 +0100

    inital commit

---
+++ assignment 2.js
@@ -0,0 +1,80 @@
+const students = [
+    {
+        first_name: "frankie",
+        last_name: "obama",
+        grade: 76,
+        age: 14,
+    },
+
+    {
+        first_name: "steve",
+        last_name: "clark",
+        grade: 50,
+        age: 16,
+    },
+
+    {
+        first_name: "monica",
+        last_name: "williams",
+        grade: 60,
+        age: 17,
+    },
+
+    {
+        first_name: "joe",
+        last_name: "benard",
+        grade: 87,
+        age: 18,
+    },
+
+    {
+        first_name: "tyler",
+        last_name: "kerry",
+        grade: 60,
+        age: 21,
+    },
+
+    {
+        first_name: "morgan",
+        last_name: "mcguire",
+        grade: 57,
+        age: 20,
+    },
+
+    {
+        first_name: "carson",
+        last_name: "steel",
+        grade: 70,
+        age: 20,
+    },
+
+    {
+        first_name: "riley",
+        last_name: "fisher",
+        grade: 45,
+        age: 20,
+    },
+
+    {
+        first_name: "hailey",
+        last_name: "stones",
+        grade: 89,
+        age: 20,
+    },
+
+    {
+        first_name: "benny",
+        last_name: "stones",
+        grade: 90,
+        age: 20,
+    },
+];
+students.sort(function (a,b) {
+    if (a.grade < b.grade) {
+        return 1
+    } else {
+        return -1
+    }
+});
+
+console.log(students)
